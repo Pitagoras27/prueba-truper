567bb0194eaf76ecf1714b62c8103dc3c08b7c4e
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-08-05 13:16:18 -0500

    Se pinta data de api en UI, se agrega componente loader para controlar los estados de la peticion

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,24 +1,16 @@
 import React from "react";
 
 import useFetchApi from "../hooks/useFetchApi";
+import Loader from "../components/Loader";
 import Cards from "../components/Cards";
 
 const Home = () => {
-  const fetchData = useFetchApi("https://api.datos.gob.mx/v1/calidadAire");
-
-  const { data: results } = fetchData;
-  const dataCards = results || {};
-  const result = Object.values(dataCards);
-  console.log(result, "<<");
-  return (
-    <>
-      {result.length > 0 &&
-        result[1].map((item) => {
-          <div>data: {item}</div>;
-        })}
-    </>
+  const { loading, data, error } = useFetchApi(
+    "https://api.datos.gob.mx/v1/calidadAire"
   );
-  // <Cards data={Object.values(dataCards)} />;
+
+  if (loading) return <Loader />;
+  if (data.results.length > 0 > 0) return <Cards data={data} />;
 };
 
 export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -20,11 +21,23 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCSSExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new MiniCSSExtractPlugin({
+      filename: "assets/[name].css",
     }),
   ],
 };
